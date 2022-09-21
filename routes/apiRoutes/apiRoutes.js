1468004d9a6508a653035b6684e5ad16fc9c3db9
Raenique27 <raeniquewalters@gmail.com>
2022-09-21 07:20:46 -1000

    updated htmlRoutes and set up apiRoutes

---
+++ routes/apiRoutes/apiRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const data = require('../db/data.js');
+
+router.get('/notes', (req, res) => {
+
+});
+
+router.post('/notes', (req, res) => {
+
+});
+
+module.exports = router;
