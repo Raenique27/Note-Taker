eb656e046208eb5b62f36e3ee1257cb7c3e269f6
Raenique27 <raeniquewalters@gmail.com>
2022-09-21 07:10:18 -1000

    add routes to htmlRouter.js file and removed it from server.js file

--- server.js
+++ server.js
@@ -11,14 +11,6 @@ app.use(express.json());
 const { db } = require('./db/db.json');
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-});
-app.post('')
-
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
