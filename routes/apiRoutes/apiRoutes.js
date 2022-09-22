c8aeaae37aa54883285f129ad11a7985d27abdc1
Raenique27 <raeniquewalters@gmail.com>
2022-09-21 16:34:21 -1000

    added POST route

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -14,7 +14,14 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
-
+    storeData
+        .addNotes(req.body)
+        .then((note) => {
+            res.json(note);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
