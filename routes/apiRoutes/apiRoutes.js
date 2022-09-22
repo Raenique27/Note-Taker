6911127254452d2dce81cdacf60df93d5fda2f38
Raenique27 <raeniquewalters@gmail.com>
2022-09-21 16:30:19 -1000

    added function to GET notes from server

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
-const data = require('../db/data.js');
+const storeData = require('../db/storeData.js');
 
 router.get('/notes', (req, res) => {
+    storeData
+        .getNotes()
+        .then((notes) => {
+            return res.json(notes);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
 
 });
 
