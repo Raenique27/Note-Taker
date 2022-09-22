db4e7d03c4d4d087b8cb0ddea3ab641fd634990e
Raenique27 <raeniquewalters@gmail.com>
2022-09-21 18:39:41 -1000

    added to server.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express(); 
@@ -7,7 +11,8 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended:true}));
 // parse incoming JSON data
 app.use(express.json());
-const { db } = require('./db/db.json');
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {
