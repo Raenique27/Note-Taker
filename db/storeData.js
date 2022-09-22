5756698f0ca3da1ea062e9e6bdd5bcd57486dbba
Raenique27 <raeniquewalters@gmail.com>
2022-09-21 18:32:26 -1000

    added function to add notes

--- db/storeData.js
+++ db/storeData.js
@@ -23,6 +23,23 @@ class Save {
             return parsedNotes;
         });
     }
+    addNotes(note) {
+        const {title, text} = note;
+        if(!title || !text) {
+            throw new Error('Please enter a title and text!');
+        }
+        const newNote = {
+            title, 
+            text, 
+            id: uuidv4()
+        };
+
+        return this.getNotes()
+            .then(notes => [...notes, newNote])
+            .then(updatedNotes => this.write(updatedNotes))
+            .then(() => newNote);
+    }
+
 
 };
 
