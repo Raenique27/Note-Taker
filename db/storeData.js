a2e72dd6b72f2aeb1e599369ab68d04d9a73a87d
Raenique27 <raeniquewalters@gmail.com>
2022-09-21 17:58:58 -1000

    created class to handle notes and created function to read notes

--- db/storeData.js
+++ db/storeData.js
@@ -1,3 +1,15 @@
 const fs = require('fs');
 const util = require('util');
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+class Save {
+    write(note) {
+        return writeNote('db/db.json', JSON.stringify(note));
+    }
+    
+};
+
+
